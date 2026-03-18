a662843ae6c60096b6e7f019cad58a3dc74c0eaf
Sonya Rashkovan <sonya.rashkovan@gmail.com>
2026-03-18 14:49:39 -0400

    Create Game_Launcher_Final.m

---
+++ Game_Launcher_Final.m
@@ -0,0 +1,14 @@
+% entry point into the game 
+function Bee_Launch()
+    spelling_bee_ui_draft();
+end
+
+% launch in Matlab app 
+% Bee_Launch()
+
+% make sure when launching you have all the files in one folder: 
+% Bee_Launch.m
+% spelling_bee_ui_draft.m 
+% word_list_puzzle.m
+% word_validation.m
+% scoring4project.m
